61c095c998c27fdc8d5e61049c27df690e661228
Deep Majumder <deep.majumder2019@gmail.com>
2020-06-29 14:04:28 +0530

    Added boilerplate code for key input

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,59 @@
+use cp_tui::event_lib::{Event, Events};
+use std::error::Error;
 use std::io;
+use termion::event::Key;
+use termion::input::MouseTerminal;
 use termion::raw::IntoRawMode;
+use termion::screen::AlternateScreen;
 use tui::backend::TermionBackend;
 use tui::layout::{Alignment, Constraint, Direction, Layout};
 use tui::style::{Color, Style};
 use tui::widgets::{Block, Borders, Paragraph, Text};
 use tui::Terminal;
 
-fn main() -> Result<(), io::Error> {
+fn main() -> Result<(), Box<dyn Error>> {
     let stdout = io::stdout().into_raw_mode()?;
+    let stdout = MouseTerminal::from(stdout);
+    let stdout = AlternateScreen::from(stdout);
     let backend = TermionBackend::new(stdout);
     let mut terminal = Terminal::new(backend)?;
-    terminal.draw(|mut f| {
-        let chunks = Layout::default()
-            .direction(Direction::Vertical)
-            .margin(1)
-            .constraints(
-                [
-                    Constraint::Percentage(10),
-                    Constraint::Percentage(80),
-                    Constraint::Percentage(10),
-                ]
-                .as_ref(),
-            )
-            .split(f.size());
-        let header_text = [Text::styled(
-            "This is my cool TUI",
-            Style::default().fg(Color::Red),
-        )];
-        let header_paragraph = Paragraph::new(header_text.iter())
-            .block(Block::default().title("Header").borders(Borders::ALL))
-            .style(Style::default().fg(Color::White).bg(Color::Black))
-            .alignment(Alignment::Center)
-            .wrap(true);
-        f.render_widget(header_paragraph, chunks[0]);
-        let block = Block::default().title("Block 2").borders(Borders::ALL);
-        f.render_widget(block, chunks[1]);
-    })
+    terminal.hide_cursor()?;
+
+    let events = Events::new();
+
+    loop {
+        terminal.draw(|mut f| {
+            let chunks = Layout::default()
+                .direction(Direction::Vertical)
+                .margin(1)
+                .constraints(
+                    [
+                        Constraint::Percentage(10),
+                        Constraint::Percentage(80),
+                        Constraint::Percentage(10),
+                    ]
+                    .as_ref(),
+                )
+                .split(f.size());
+            let header_text = [Text::styled(
+                "This is my cool TUI",
+                Style::default().fg(Color::Red),
+            )];
+            let header_paragraph = Paragraph::new(header_text.iter())
+                .block(Block::default().title("Header").borders(Borders::ALL))
+                .style(Style::default().fg(Color::White).bg(Color::Black))
+                .alignment(Alignment::Center)
+                .wrap(true);
+            f.render_widget(header_paragraph, chunks[0]);
+            let block = Block::default().title("Block 2").borders(Borders::ALL);
+            f.render_widget(block, chunks[1]);
+        })?;
+
+        if let Event::Input(input) = events.next()? {
+            if let Key::Char('q') = input {
+                break;
+            }
+        }
+    }
+    Ok(())
 }
