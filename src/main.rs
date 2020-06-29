3f4c9a8c470632ddab8c041406e163afaf857857
Deep Majumder <deep.majumder2019@gmail.com>
2020-06-29 13:28:47 +0530

    Initial boilerplate code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+use termion::raw::IntoRawMode;
+use tui::backend::TermionBackend;
+use tui::layout::{Alignment, Constraint, Direction, Layout};
+use tui::style::{Color, Style};
+use tui::widgets::{Block, Borders, Paragraph, Text};
+use tui::Terminal;
+
+fn main() -> Result<(), io::Error> {
+    let stdout = io::stdout().into_raw_mode()?;
+    let backend = TermionBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+    terminal.draw(|mut f| {
+        let chunks = Layout::default()
+            .direction(Direction::Vertical)
+            .margin(1)
+            .constraints(
+                [
+                    Constraint::Percentage(10),
+                    Constraint::Percentage(80),
+                    Constraint::Percentage(10),
+                ]
+                .as_ref(),
+            )
+            .split(f.size());
+        let header_text = [Text::styled(
+            "This is my cool TUI",
+            Style::default().fg(Color::Red),
+        )];
+        let header_paragraph = Paragraph::new(header_text.iter())
+            .block(Block::default().title("Header").borders(Borders::ALL))
+            .style(Style::default().fg(Color::White).bg(Color::Black))
+            .alignment(Alignment::Center)
+            .wrap(true);
+        f.render_widget(header_paragraph, chunks[0]);
+        let block = Block::default().title("Block 2").borders(Borders::ALL);
+        f.render_widget(block, chunks[1]);
+    })
 }
